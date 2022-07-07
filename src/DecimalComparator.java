a62986162801efcdac6c44f91fefba0b0f3941fb
durmuserdogan <drmserdogan@outlook.com>
2022-07-07 10:07:02 +0300

    second

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,7 +1,7 @@
 public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces(double one, double two){
-        double oneNum = one*1000;
-        double twoNum = two*1000;
+        double oneNum = one*10000;
+        double twoNum = two*10000;
 
         if ((int)oneNum==twoNum){
             return true;
